6d75beb8a3483aacb7fe33b954e89ec7695de835
Chung Chi Yeung <chi12275a@gmail.com>
2026-03-19 15:16:07 +0000

    03/19/2026 update Feature: Add formatVTReport function to print VT report output

--- go.mod
+++ go.mod
@@ -3,11 +3,11 @@ module github.com/YccYeung/LightTI
 go 1.19
 
 require (
-	github.com/VirusTotal/vt-go v1.1.0 // indirect
-	github.com/WqyJh/go-fstring v0.4.1 // indirect
+	github.com/joho/godotenv v1.5.1
+	github.com/spf13/cobra v1.10.2
+)
+
+require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/spf13/cobra v1.10.2 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
-	github.com/thedevsaddam/gojsonq/v2 v2.5.2 // indirect
 )
--- internal/enricher/ipEnricher.go
+++ internal/enricher/ipEnricher.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"encoding/json"
 	"io"
+	"strings"
 
 	"github.com/joho/godotenv"
 )
@@ -15,18 +16,20 @@ import (
 // that are relevant for threat intelligence analysis.
 type VTResult struct {
 	Data struct {
+		ID    string `json:"id"`
+		Type  string `json:"type"`
 		Attributes struct {
 			Tags						[]string	`json:"tags"`
-			Continent            		string `json:"continent"`
+			Continent            		string 		`json:"continent"`
 			Country 					string  	`json:"country"`
 			ASN							int			`json:"asn"`
 			AsOwner						string 		`json:"as_owner"`
 			Network						string		`json:"network"`
 			RegionalInternetRegistry	string 		`json:"regional_internet_registry"`
 			LastHTTPSCertificateDate	int			`json:"last_https_certificate_date"`
-			WhoisDate                	int `json:"whois_date"`
-			LastModificationDate     	int `json:"last_modification_date"`
-			Reputation 					int `json:"reputation"`
+			WhoisDate                	int 		`json:"whois_date"`
+			LastModificationDate     	int 		`json:"last_modification_date"`
+			Reputation 					int 		`json:"reputation"`
 			TotalVotes               	struct {
 				Harmless  int `json:"harmless"`
 				Malicious int `json:"malicious"`
@@ -44,7 +47,7 @@ type VTResult struct {
 
 // sanitizeVTOutput parses a raw VirusTotal JSON response into a VTResult struct.
 // Returns an error if the JSON is malformed or cannot be decoded.
-func sanitizeVTOutput(report string) (VTResult, error) {
+func parseVTOutput(report string) (VTResult, error) {
 	var vtReport VTResult
 	
 	err := json.Unmarshal([]byte(report), &vtReport)
@@ -53,6 +56,40 @@ func sanitizeVTOutput(report string) (VTResult, error) {
 	}
 
 	return vtReport, nil
+}
+
+func formatVTReport(report VTResult) string {
+	d := report.Data
+	a := d.Attributes
+
+	output := "\n=== VirusTotal Report ===\n\n"
+
+	output += "General Information\n"
+	output += fmt.Sprintf("  %-30s %s\n", "IP Address:", d.ID)
+	output += fmt.Sprintf("  %-30s %s\n", "Network:", a.Network)
+	output += fmt.Sprintf("  %-30s %s\n", "Country:", a.Country)
+	output += fmt.Sprintf("  %-30s %s\n", "Continent:", a.Continent)
+	output += fmt.Sprintf("  %-30s %d\n", "ASN:", a.ASN)
+	output += fmt.Sprintf("  %-30s %s\n", "AS Owner:", a.AsOwner)
+	output += fmt.Sprintf("  %-30s %s\n", "Regional Internet Registry:", a.RegionalInternetRegistry)
+	output += fmt.Sprintf("  %-30s %d\n", "Reputation:", a.Reputation)
+
+	if len(a.Tags) > 0 {
+		output += fmt.Sprintf("  %-30s %s\n", "Tags:", strings.Join(a.Tags, ", "))
+	}
+
+	output += "\nLast Analysis Stats\n"
+	output += fmt.Sprintf("  %-30s %d\n", "Malicious:", a.LastAnalysisStats.Malicious)
+	output += fmt.Sprintf("  %-30s %d\n", "Suspicious:", a.LastAnalysisStats.Suspicious)
+	output += fmt.Sprintf("  %-30s %d\n", "Harmless:", a.LastAnalysisStats.Harmless)
+	output += fmt.Sprintf("  %-30s %d\n", "Undetected:", a.LastAnalysisStats.Undetected)
+	output += fmt.Sprintf("  %-30s %d\n", "Timeout:", a.LastAnalysisStats.Timeout)
+
+	output += "\nTotal Votes\n"
+	output += fmt.Sprintf("  %-30s %d\n", "Malicious:", a.TotalVotes.Malicious)
+	output += fmt.Sprintf("  %-30s %d\n", "Harmless:", a.TotalVotes.Harmless)
+
+	return output
 }
 
 // EnrichIP looks up threat intelligence for the given IP address across
@@ -81,11 +118,11 @@ func EnrichIP(ip string) {
 	defer vtRes.Body.Close()
 	vtBody, _ := io.ReadAll(vtRes.Body)
 
-	vtReport, err := sanitizeVTOutput(string(vtBody))
+	vtReport, err := parseVTOutput(string(vtBody))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to parse VT report: %v\n", err)
 		return
 	}
 
-	fmt.Println(vtReport)
+	fmt.Printf(formatVTReport(vtReport))
 }
