6139f98b09a5443ed2ceaf337d114a76bb4940b5
Chung Chi Yeung <chi12275a@gmail.com>
2026-03-22 21:41:02 +0000

    feature: add PostgreSQL store layer with SaveLookup and SaveResult

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/YccYeung/LightTI
 
-go 1.24.0
+go 1.25.0
 
 require (
 	github.com/joho/godotenv v1.5.1
@@ -9,6 +9,11 @@ require (
 
 require (
 	github.com/golang-migrate/migrate/v4 v4.19.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.9.1 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
+	golang.org/x/text v0.31.0 // indirect
 )
---
+++ internal/store/store.go
@@ -0,0 +1,42 @@
+package store
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Store interface {
+	SaveLookup(ctx context.Context, ioc string, iocType string) (uuid.UUID, error)
+	SaveResult(ctx context.Context, lookupID uuid.UUID, source string, result []byte, err string) error
+}
+
+type Postgres struct {
+	pool *pgxpool.Pool
+}
+
+func New(ctx context.Context, dbURL string) (*Postgres, error) {
+	pool, err := pgxpool.New(ctx, dbURL)
+	if err != nil {
+		return nil, err
+	}
+	return &Postgres{pool: pool}, nil
+}
+
+func (p *Postgres) SaveLookup(ctx context.Context, ioc string, iocType string) (uuid.UUID, error) {
+	var id uuid.UUID 
+	err := p.pool.QueryRow(ctx, "INSERT INTO lookups (ioc, ioc_type) VALUES ($1, $2) RETURNING id", ioc, iocType).Scan(&id)
+	if err != nil {
+		return uuid.UUID{}, err
+	} 
+	return id, nil
+}
+
+func (p *Postgres) SaveResult(ctx context.Context, lookupID uuid.UUID, source string, result []byte, errMsg string) error {
+	_, err := p.pool.Exec(ctx, "INSERT INTO enrichment_results (lookup_id, source, result, error) VALUES ($1, $2, $3, $4)", lookupID, source, , errMsg)
+	if err != nil {
+		return err
+	} 
+	return nil
+}
